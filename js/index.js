9b366d6d31b2cfc2787d50e1d8d5df56d2a38eab
Cameron Lares <cameronlares@gmail.com>
2020-05-31 21:22:17 -0400

    Added nav,about and features

--- js/index.js
+++ js/index.js
@@ -39,4 +39,35 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent.nav["img-src"]) // second arg is the variable. nav is object, img-src is value of that object[]
+
+
+let navNode = document.querySelector("nav")
+
+console.log(siteContent.nav["nav-item-1"])
+
+
+//Nav
+navNode.childNodes[1].textContent=siteContent["nav"]["nav-item-1"]
+navNode.childNodes[3].textContent=siteContent["nav"]["nav-item-2"]
+navNode.childNodes[5].textContent=siteContent["nav"]["nav-item-3"]
+navNode.childNodes[7].textContent=siteContent["nav"]["nav-item-4"]
+navNode.childNodes[9].textContent=siteContent["nav"]["nav-item-5"]
+navNode.childNodes[11].textContent=siteContent["nav"]["nav-item-6"]
+//Nav
+
+let textContent_1= document.querySelector(".top-content .text-content:nth-child(1)");
+let textContent_2= document.querySelector(".top-content .text-content:nth-child(2)");
+
+
+
+textContent_1.children[0].textContent=siteContent['main-content']['features-h4'];
+textContent_1.children[1].textContent=siteContent['main-content']['features-content'];
+
+textContent_2.children[0].textContent=siteContent['main-content']['about-h4'];
+textContent_2.children[1].textContent=siteContent['main-content']['about-content'];
+
+console.log(textContent_1)
+
+
